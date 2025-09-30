90dc226eee9df21584136e7da87d52a19882c7c3
truongvvjune2003 <truongvvjune2003@gmail.com>
2025-09-30 23:07:22 +0700

    M models

--- backend/models/DoctorSchedule.js
+++ backend/models/DoctorSchedule.js
@@ -8,12 +8,19 @@ const doctorScheduleSchema = new mongoose.Schema({
         type: Date,
         required: true
     },
-    startTime: {
-        type: String
-    },
-    endTime: {
-        type: String
-    },
+    timeSlots: [
+        {
+            startTime: { type: String, required: true },   // "07:00"
+            endTime: { type: String, required: true },     // "08:00"
+            capacity: { type: Number, default: 1 },        // số bệnh nhân tối đa
+            bookedCount: { type: Number, default: 0 },     // đã confirm bao nhiêu người
+            status: {
+                type: String,
+                enum: ["available", "full", "closed"],       // slot mở, đã full, hoặc đóng
+                default: "available"
+            }
+        }
+    ],
     specialty: {
         type: String
     },
